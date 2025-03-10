d384500233309834ba27d0da6b0c3cb0d7719fea
Hasitha.Kaluarachchi <featuretechnologys@gmail.com>
2025-03-10 22:08:28 +0530

    create entryfield

--- cal.py
+++ cal.py
@@ -2,5 +2,10 @@ from tkinter import *
 
 root=Tk()
 root.title('smart calculator')
+root.config(bg='deepskyblue2')
+root.geometry('680x486+100+100')
+
+entryField=Entry(root,font=('arial',20,'bold'),bg='deepskyblue2',fg='white',bd=10,relief=SUNKEN,width=30)
+entryField.grid(row=0,column=0)
 
 root.mainloop() 
